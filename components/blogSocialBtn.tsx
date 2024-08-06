0c4e2dcffc956681da58c75595309f4facc76e97
0xpranv <pranav@greendreamearth.org>
2024-08-06 16:06:27 +0530

    changes changes

--- components/blogSocialBtn.tsx
+++ components/blogSocialBtn.tsx
@@ -11,18 +11,18 @@ import {
 } from "react-share";
 
 export const BlogSocialButtonGroup = ({ blog }: { blog: any }) => {
-  console.log("from blogSocialbtns ",blog);
-  const pathname = usePathname();
-  console.log(pathname);
+  console.log("from blogSocialbtns ",blog?.title.rendered);
+  const pathname = "https://helpstudyabroad.com" + usePathname();
+  console.log("ppathname ",pathname);
   return (
     <div className="flex items-center space-x-2">
-      <LinkedinShareButton title={blog?.title} url={pathname || ""}>
+      <LinkedinShareButton title={blog?.title.rendered} url={pathname || ""}>
         <LinkedinIcon size={32} round={true} />
       </LinkedinShareButton>
-      <FacebookShareButton title={blog?.title} url={pathname || ""}>
+      <FacebookShareButton title={blog?.title.rendered} url={pathname || ""}>
         <FacebookIcon size={32} round={true} />
       </FacebookShareButton>
-      <WhatsappShareButton title={blog?.title} url={pathname || ""}>
+      <WhatsappShareButton title={blog?.title.rendered} url={pathname || ""}>
         <WhatsappIcon size={32} round={true} />
       </WhatsappShareButton>
     </div>

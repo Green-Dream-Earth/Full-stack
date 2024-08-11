6c8b3a650e87e87d3c3c15ed8f60d578f3d76a07
0xpranv <pranav@greendreamearth.org>
2024-08-12 01:17:26 +0530

    reduced width of the faq elements

--- components/faq.tsx
+++ components/faq.tsx
@@ -9,15 +9,15 @@ import { Button } from "./button";
 
 export function Faq() {
   return (
-    <section className="">
+    <section className="container">
       <div className="text-center mb-14">
         <h2 className="text-gray-600 text-center font-bold text-3xl w-full mx-auto">
           Frequently Asked Questions
         </h2>
       </div>
-      <Accordion type="single" collapsible className="container">
+      <Accordion type="single" collapsible className="flex flex-col justify-center">
         {faqs.map((faq) => (
-          <AccordionItem key={faq.id} value={`item-${faq.id}`}>
+          <AccordionItem className="max-w-4xl w-full" key={faq.id} value={`item-${faq.id}`}>
             <AccordionTrigger className="text-left">
               {faq.question}
             </AccordionTrigger>
